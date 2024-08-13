2bf5b933af5ec756254a15072e7e6bffb0391150
Prince Stephen <princeinkunzi@gmail.com>
2024-08-13 07:46:05 +0300

    Delete my-client directory

--- my-client/src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    apiBaseUrl: 'http://localhost:5000', // Backend API URL
-  };
-  
-  export default config;
-  
--- my-client/src/utils.js
+++
@@ -1,5 +0,0 @@
-export const getToken = () => {return localStorage.getItem('access_token');};
-
-export const setToken = (token) => localStorage.setItem('token', token);
-
-export const removeToken = () => localStorage.removeItem('token');
